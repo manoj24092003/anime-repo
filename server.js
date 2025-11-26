90956bd76148a3b0d06b7c096f067cae9adf530c
manoj24092003 <146913136+manoj24092003@users.noreply.github.com>
2025-11-27 00:09:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const request = require("request");
+const https = require("https");
+
 const app = express();
 
 app.get("/", (req, res) => {
@@ -10,25 +11,23 @@ app.get("/stream", (req, res) => {
   const fileUrl = req.query.url;
   if (!fileUrl) return res.status(400).send("Missing url param");
 
-  request
-    .get({
-      url: fileUrl,
-      headers: {
-        "Range": req.headers.range || "",
-        "User-Agent": "Mozilla/5.0"
-      }
-    })
-    .on("response", (remoteRes) => {
-      res.writeHead(remoteRes.statusCode, {
-        "Content-Type": remoteRes.headers["content-type"] || "video/mp4",
-        "Content-Length": remoteRes.headers["content-length"],
-        ...(remoteRes.headers["content-range"]
-          ? { "Content-Range": remoteRes.headers["content-range"] }
-          : {})
-      });
-    })
-    .pipe(res);
+  https.get(fileUrl, { headers: { "Range": req.headers.range || "" } }, (tgRes) => {
+    res.writeHead(tgRes.statusCode, {
+      "Content-Type": tgRes.headers["content-type"] || "video/mp4",
+      "Content-Length": tgRes.headers["content-length"],
+      "Accept-Ranges": "bytes",
+      ...(tgRes.headers["content-range"]
+        ? { "Content-Range": tgRes.headers["content-range"] }
+        : {})
+    });
+
+    tgRes.pipe(res);
+  }).on("error", () => {
+    res.sendStatus(500);
+  });
 });
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log("Running on " + PORT));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Proxy running on port", PORT);
+});
